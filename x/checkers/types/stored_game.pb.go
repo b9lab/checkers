cosmos/b9-checkers-academy-draft

Add wager denom and tests.

x/checkers/types/stored_game.pb.go
@@ -34,6 +34,7 @@ type StoredGame struct {
 	Deadline    string `protobuf:"bytes,9,opt,name=deadline,proto3" json:"deadline,omitempty"`
 	Winner      string `protobuf:"bytes,10,opt,name=winner,proto3" json:"winner,omitempty"`
 	Wager       uint64 `protobuf:"varint,11,opt,name=wager,proto3" json:"wager,omitempty"`
+	Denom       string `protobuf:"bytes,12,opt,name=denom,proto3" json:"denom,omitempty"`
 }
 
 func (m *StoredGame) Reset()         { *m = StoredGame{} }
@@ -146,6 +147,13 @@ func (m *StoredGame) GetWager() uint64 {
 	return 0
 }
 
+func (m *StoredGame) GetDenom() string {
+	if m != nil {
+		return m.Denom
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*StoredGame)(nil), "b9lab.checkers.checkers.StoredGame")
 }
@@ -153,26 +161,26 @@ func init() {
 func init() { proto.RegisterFile("checkers/stored_game.proto", fileDescriptor_8439c9c90688ff75) }
 
 var fileDescriptor_8439c9c90688ff75 = []byte{
-	// 295 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x4c, 0x91, 0xbd, 0x4e, 0x32, 0x41,
-	0x14, 0x86, 0x19, 0xfe, 0x3e, 0x38, 0x34, 0x5f, 0x26, 0x46, 0x27, 0xc4, 0x4c, 0x88, 0x15, 0xb1,
-	0x80, 0xc2, 0xca, 0x56, 0x4d, 0x8c, 0x2d, 0x76, 0x36, 0x66, 0x66, 0xe7, 0x00, 0x1b, 0xd8, 0x19,
-	0x32, 0x3b, 0x2b, 0x78, 0x17, 0x5e, 0x81, 0xd7, 0x63, 0x49, 0x69, 0x69, 0x76, 0x6f, 0xc4, 0xec,
-	0x59, 0x5c, 0xe8, 0xde, 0xf7, 0x39, 0xcf, 0x24, 0x27, 0x73, 0x60, 0x18, 0x2d, 0x31, 0x5a, 0xa1,
-	0x4f, 0xa7, 0x69, 0x70, 0x1e, 0xcd, 0xeb, 0x42, 0x25, 0x38, 0xd9, 0x78, 0x17, 0x1c, 0xbf, 0xd0,
-	0xb7, 0x6b, 0xa5, 0x27, 0x7f, 0x46, 0x1d, 0xae, 0x3e, 0x9b, 0x00, 0xcf, 0xa4, 0x3f, 0xaa, 0x04,
-	0xf9, 0x19, 0x74, 0x62, 0x6b, 0x70, 0x27, 0xd8, 0x88, 0x8d, 0xfb, 0xb3, 0xaa, 0x94, 0x54, 0x3b,
-	0xe5, 0x8d, 0x68, 0x56, 0x94, 0x0a, 0xe7, 0xd0, 0x0e, 0x99, 0xb7, 0xa2, 0x45, 0x90, 0x32, 0x99,
-	0x6b, 0x15, 0xad, 0x44, 0xfb, 0x60, 0x96, 0x85, 0xff, 0x87, 0x96, 0x47, 0x23, 0x3a, 0xc4, 0xca,
-	0xc8, 0x2f, 0xa1, 0x9f, 0xb8, 0x37, 0xbc, 0x77, 0x99, 0x0d, 0xa2, 0x3b, 0x62, 0xe3, 0xf6, 0xec,
-	0x08, 0xf8, 0x08, 0x06, 0x1a, 0xe7, 0xce, 0xe3, 0x13, 0xed, 0xf2, 0x8f, 0xde, 0x9d, 0x22, 0x2e,
-	0x01, 0xd4, 0x3c, 0xa0, 0xaf, 0x84, 0x1e, 0x09, 0x27, 0x84, 0x0f, 0xa1, 0x67, 0x50, 0x99, 0x75,
-	0x6c, 0x51, 0xf4, 0x69, 0x5a, 0x77, 0x7e, 0x0e, 0xdd, 0x6d, 0x6c, 0x2d, 0x7a, 0x01, 0x34, 0x39,
-	0xb4, 0x72, 0xf7, 0xad, 0x5a, 0xa0, 0x17, 0x03, 0xda, 0xa7, 0x2a, 0x77, 0x0f, 0x5f, 0xb9, 0x64,
-	0xfb, 0x5c, 0xb2, 0x9f, 0x5c, 0xb2, 0x8f, 0x42, 0x36, 0xf6, 0x85, 0x6c, 0x7c, 0x17, 0xb2, 0xf1,
-	0x72, 0xbd, 0x88, 0xc3, 0x32, 0xd3, 0x93, 0xc8, 0x25, 0x53, 0xfa, 0xde, 0x69, 0x7d, 0x80, 0xdd,
-	0x31, 0x86, 0xf7, 0x0d, 0xa6, 0xba, 0x4b, 0x67, 0xb8, 0xf9, 0x0d, 0x00, 0x00, 0xff, 0xff, 0xf4,
-	0xc4, 0x57, 0x13, 0xa4, 0x01, 0x00, 0x00,
+	// 304 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x4c, 0x91, 0xbf, 0x4e, 0x02, 0x41,
+	0x10, 0xc6, 0x59, 0xfe, 0x09, 0x83, 0x85, 0xd9, 0x18, 0xdd, 0x10, 0xb3, 0x21, 0x56, 0xc4, 0x02,
+	0x0a, 0x2b, 0x5b, 0x35, 0x31, 0xb6, 0xd8, 0xd9, 0x98, 0xdd, 0xdb, 0x01, 0x2e, 0x70, 0xbb, 0x64,
+	0xd9, 0x13, 0x7c, 0x0b, 0x1f, 0xc6, 0x87, 0xb0, 0xa4, 0xb4, 0x34, 0x77, 0x2f, 0x62, 0x6e, 0x0e,
+	0x0f, 0xba, 0xef, 0xf7, 0x9b, 0xef, 0x92, 0xb9, 0x1d, 0xe8, 0x47, 0x73, 0x8c, 0x16, 0xe8, 0xd7,
+	0xe3, 0x75, 0x70, 0x1e, 0xcd, 0xdb, 0x4c, 0x25, 0x38, 0x5a, 0x79, 0x17, 0x1c, 0xbf, 0xd4, 0x77,
+	0x4b, 0xa5, 0x47, 0xff, 0x8d, 0x2a, 0x5c, 0x7f, 0xd5, 0x01, 0x5e, 0xa8, 0xfe, 0xa4, 0x12, 0xe4,
+	0xe7, 0xd0, 0x8a, 0xad, 0xc1, 0xad, 0x60, 0x03, 0x36, 0xec, 0x4e, 0x4a, 0x28, 0xac, 0x76, 0xca,
+	0x1b, 0x51, 0x2f, 0x2d, 0x01, 0xe7, 0xd0, 0x0c, 0xa9, 0xb7, 0xa2, 0x41, 0x92, 0x32, 0x35, 0x97,
+	0x2a, 0x5a, 0x88, 0xe6, 0xbe, 0x59, 0x00, 0x3f, 0x83, 0x86, 0x47, 0x23, 0x5a, 0xe4, 0x8a, 0xc8,
+	0xaf, 0xa0, 0x9b, 0xb8, 0x77, 0x7c, 0x70, 0xa9, 0x0d, 0xa2, 0x3d, 0x60, 0xc3, 0xe6, 0xe4, 0x20,
+	0xf8, 0x00, 0x7a, 0x1a, 0xa7, 0xce, 0xe3, 0x33, 0xed, 0x72, 0x42, 0xdf, 0x1d, 0x2b, 0x2e, 0x01,
+	0xd4, 0x34, 0xa0, 0x2f, 0x0b, 0x1d, 0x2a, 0x1c, 0x19, 0xde, 0x87, 0x8e, 0x41, 0x65, 0x96, 0xb1,
+	0x45, 0xd1, 0xa5, 0x69, 0xc5, 0xfc, 0x02, 0xda, 0x9b, 0xd8, 0x5a, 0xf4, 0x02, 0x68, 0xb2, 0xa7,
+	0x62, 0xf7, 0x8d, 0x9a, 0xa1, 0x17, 0x3d, 0xda, 0xa7, 0x84, 0xc2, 0x1a, 0xb4, 0x2e, 0x11, 0xa7,
+	0xe5, 0x1f, 0x11, 0xdc, 0x3f, 0x7e, 0x67, 0x92, 0xed, 0x32, 0xc9, 0x7e, 0x33, 0xc9, 0x3e, 0x73,
+	0x59, 0xdb, 0xe5, 0xb2, 0xf6, 0x93, 0xcb, 0xda, 0xeb, 0xcd, 0x2c, 0x0e, 0xf3, 0x54, 0x8f, 0x22,
+	0x97, 0x8c, 0xe9, 0xd1, 0xc7, 0xd5, 0x59, 0xb6, 0x87, 0x18, 0x3e, 0x56, 0xb8, 0xd6, 0x6d, 0x3a,
+	0xce, 0xed, 0x5f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x87, 0x33, 0xf4, 0xb3, 0xba, 0x01, 0x00, 0x00,
 }
 
 func (m *StoredGame) Marshal() (dAtA []byte, err error) {
@@ -195,6 +203,13 @@ func (m *StoredGame) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.Denom) > 0 {
+		i -= len(m.Denom)
+		copy(dAtA[i:], m.Denom)
+		i = encodeVarintStoredGame(dAtA, i, uint64(len(m.Denom)))
+		i--
+		dAtA[i] = 0x62
+	}
 	if m.Wager != 0 {
 		i = encodeVarintStoredGame(dAtA, i, uint64(m.Wager))
 		i--
@@ -329,6 +344,10 @@ func (m *StoredGame) Size() (n int) {
 	}
 	if m.Wager != 0 {
 		n += 1 + sovStoredGame(uint64(m.Wager))
+	}
+	l = len(m.Denom)
+	if l > 0 {
+		n += 1 + l + sovStoredGame(uint64(l))
 	}
 	return n
 }
@@ -694,6 +713,38 @@ func (m *StoredGame) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
+		case 12:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Denom", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowStoredGame
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthStoredGame
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthStoredGame
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Denom = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipStoredGame(dAtA[iNdEx:])

b9lab/checkers

x/checkers/types/stored_game.pb.go
@@ -34,6 +34,7 @@ type StoredGame struct {
 	BeforeIndex string `protobuf:"bytes,9,opt,name=beforeIndex,proto3" json:"beforeIndex,omitempty"`
 	AfterIndex  string `protobuf:"bytes,10,opt,name=afterIndex,proto3" json:"afterIndex,omitempty"`
 	Wager       uint64 `protobuf:"varint,11,opt,name=wager,proto3" json:"wager,omitempty"`
+	Denom       string `protobuf:"bytes,12,opt,name=denom,proto3" json:"denom,omitempty"`
 }
 
 func (m *StoredGame) Reset()         { *m = StoredGame{} }
@@ -146,6 +147,13 @@ func (m *StoredGame) GetWager() uint64 {
 	return 0
 }
 
+func (m *StoredGame) GetDenom() string {
+	if m != nil {
+		return m.Denom
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*StoredGame)(nil), "b9lab.checkers.checkers.StoredGame")
 }
@@ -153,26 +161,26 @@ func init() {
 func init() { proto.RegisterFile("checkers/stored_game.proto", fileDescriptor_8439c9c90688ff75) }
 
 var fileDescriptor_8439c9c90688ff75 = []byte{
-	// 295 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x4c, 0x91, 0xcd, 0x4e, 0x32, 0x31,
-	0x14, 0x86, 0x29, 0x7f, 0x1f, 0x1c, 0x36, 0x5f, 0x1a, 0xa3, 0x0d, 0x31, 0x0d, 0x71, 0x45, 0x5c,
-	0xc0, 0xc2, 0x95, 0x5b, 0x35, 0x31, 0x6e, 0x71, 0xe7, 0xc6, 0xb4, 0xd3, 0x03, 0x4c, 0x60, 0x5a,
-	0xd2, 0xe9, 0x08, 0xde, 0x85, 0x57, 0xe0, 0xf5, 0xb8, 0x64, 0xe9, 0xd2, 0xcc, 0xdc, 0x88, 0x99,
-	0x33, 0x38, 0xb0, 0x7b, 0xdf, 0xa7, 0x4f, 0x9b, 0x93, 0x1e, 0x18, 0x46, 0x4b, 0x8c, 0x56, 0xe8,
-	0xd3, 0x69, 0x1a, 0x9c, 0x47, 0xf3, 0xba, 0x50, 0x09, 0x4e, 0x36, 0xde, 0x05, 0xc7, 0x2f, 0xf4,
-	0xed, 0x5a, 0xe9, 0xc9, 0x9f, 0x51, 0x87, 0xab, 0xcf, 0x26, 0xc0, 0x33, 0xe9, 0x8f, 0x2a, 0x41,
-	0x7e, 0x06, 0x9d, 0xd8, 0x1a, 0xdc, 0x09, 0x36, 0x62, 0xe3, 0xfe, 0xac, 0x2a, 0x25, 0xd5, 0x4e,
-	0x79, 0x23, 0x9a, 0x15, 0xa5, 0xc2, 0x39, 0xb4, 0x43, 0xe6, 0xad, 0x68, 0x11, 0xa4, 0x4c, 0xe6,
-	0x5a, 0x45, 0x2b, 0xd1, 0x3e, 0x98, 0x65, 0xe1, 0xff, 0xa1, 0xe5, 0xd1, 0x88, 0x0e, 0xb1, 0x32,
-	0xf2, 0x73, 0xe8, 0x6e, 0x63, 0x6b, 0xd1, 0x8b, 0x2e, 0xc1, 0x43, 0xe3, 0x43, 0xe8, 0x19, 0x54,
-	0x66, 0x1d, 0x5b, 0x14, 0xff, 0xe8, 0xa4, 0xee, 0xfc, 0x12, 0xfa, 0x89, 0x7b, 0xc3, 0x7b, 0x97,
-	0xd9, 0x20, 0x7a, 0x23, 0x36, 0x6e, 0xcf, 0x8e, 0x80, 0x8f, 0x60, 0xa0, 0x71, 0xee, 0x3c, 0x3e,
-	0xd1, 0xfc, 0x7d, 0xba, 0x7c, 0x8a, 0xb8, 0x04, 0x50, 0xf3, 0x80, 0xbe, 0x12, 0x80, 0x84, 0x13,
-	0x52, 0xce, 0xbe, 0x55, 0x0b, 0xf4, 0x62, 0x40, 0x6f, 0x57, 0xe5, 0xee, 0xe1, 0x2b, 0x97, 0x6c,
-	0x9f, 0x4b, 0xf6, 0x93, 0x4b, 0xf6, 0x51, 0xc8, 0xc6, 0xbe, 0x90, 0x8d, 0xef, 0x42, 0x36, 0x5e,
-	0xae, 0x17, 0x71, 0x58, 0x66, 0x7a, 0x12, 0xb9, 0x64, 0x4a, 0xdf, 0x3b, 0xad, 0x17, 0xb0, 0x3b,
-	0xc6, 0xf0, 0xbe, 0xc1, 0x54, 0x77, 0x69, 0x0d, 0x37, 0xbf, 0x01, 0x00, 0x00, 0xff, 0xff, 0x20,
-	0xca, 0x84, 0x6d, 0xa4, 0x01, 0x00, 0x00,
+	// 303 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x4c, 0x91, 0xcf, 0x4e, 0x02, 0x31,
+	0x10, 0xc6, 0x29, 0xff, 0x84, 0xc1, 0x83, 0x69, 0x8c, 0x36, 0xc4, 0x34, 0xc4, 0x13, 0xf1, 0x00,
+	0x07, 0x4f, 0x5e, 0xd5, 0xc4, 0x78, 0xc5, 0x9b, 0x17, 0xd3, 0x6e, 0x07, 0xd8, 0xc0, 0xb6, 0xa4,
+	0x74, 0x05, 0xdf, 0xc2, 0x87, 0xf1, 0x21, 0x3c, 0x72, 0xf4, 0x68, 0x76, 0x5f, 0xc4, 0xec, 0x2c,
+	0x2e, 0xdc, 0xbe, 0xdf, 0x6f, 0xbf, 0x6d, 0xa6, 0x1d, 0xe8, 0x47, 0x73, 0x8c, 0x16, 0xe8, 0xd7,
+	0xe3, 0x75, 0x70, 0x1e, 0xcd, 0xdb, 0x4c, 0x25, 0x38, 0x5a, 0x79, 0x17, 0x1c, 0xbf, 0xd4, 0x77,
+	0x4b, 0xa5, 0x47, 0xff, 0x8d, 0x2a, 0x5c, 0x7f, 0xd5, 0x01, 0x5e, 0xa8, 0xfe, 0xa4, 0x12, 0xe4,
+	0xe7, 0xd0, 0x8a, 0xad, 0xc1, 0xad, 0x60, 0x03, 0x36, 0xec, 0x4e, 0x4a, 0x28, 0xac, 0x76, 0xca,
+	0x1b, 0x51, 0x2f, 0x2d, 0x01, 0xe7, 0xd0, 0x0c, 0xa9, 0xb7, 0xa2, 0x41, 0x92, 0x32, 0x35, 0x97,
+	0x2a, 0x5a, 0x88, 0xe6, 0xbe, 0x59, 0x00, 0x3f, 0x83, 0x86, 0x47, 0x23, 0x5a, 0xe4, 0x8a, 0xc8,
+	0x2f, 0xa0, 0xbd, 0x89, 0xad, 0x45, 0x2f, 0xda, 0x24, 0xf7, 0xc4, 0xfb, 0xd0, 0x31, 0xa8, 0xcc,
+	0x32, 0xb6, 0x28, 0x4e, 0xe8, 0x4b, 0xc5, 0xfc, 0x0a, 0xba, 0x89, 0x7b, 0xc7, 0x07, 0x97, 0xda,
+	0x20, 0x3a, 0x03, 0x36, 0x6c, 0x4e, 0x0e, 0x82, 0x0f, 0xa0, 0xa7, 0x71, 0xea, 0x3c, 0x3e, 0xd3,
+	0xfc, 0x5d, 0xfa, 0xf9, 0x58, 0x71, 0x09, 0xa0, 0xa6, 0x01, 0x7d, 0x59, 0x00, 0x2a, 0x1c, 0x99,
+	0x62, 0xf6, 0x8d, 0x9a, 0xa1, 0x17, 0x3d, 0x3a, 0xbb, 0x84, 0xc2, 0x1a, 0xb4, 0x2e, 0x11, 0xa7,
+	0xe5, 0x8d, 0x08, 0xee, 0x1f, 0xbf, 0x33, 0xc9, 0x76, 0x99, 0x64, 0xbf, 0x99, 0x64, 0x9f, 0xb9,
+	0xac, 0xed, 0x72, 0x59, 0xfb, 0xc9, 0x65, 0xed, 0xf5, 0x66, 0x16, 0x87, 0x79, 0xaa, 0x47, 0x91,
+	0x4b, 0xc6, 0xf4, 0xe8, 0xe3, 0x6a, 0x2d, 0xdb, 0x43, 0x0c, 0x1f, 0x2b, 0x5c, 0xeb, 0x36, 0x2d,
+	0xe7, 0xf6, 0x2f, 0x00, 0x00, 0xff, 0xff, 0x0b, 0x35, 0x1b, 0xed, 0xba, 0x01, 0x00, 0x00,
 }
 
 func (m *StoredGame) Marshal() (dAtA []byte, err error) {
@@ -195,6 +203,13 @@ func (m *StoredGame) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.Denom) > 0 {
+		i -= len(m.Denom)
+		copy(dAtA[i:], m.Denom)
+		i = encodeVarintStoredGame(dAtA, i, uint64(len(m.Denom)))
+		i--
+		dAtA[i] = 0x62
+	}
 	if m.Wager != 0 {
 		i = encodeVarintStoredGame(dAtA, i, uint64(m.Wager))
 		i--
@@ -329,6 +344,10 @@ func (m *StoredGame) Size() (n int) {
 	}
 	if m.Wager != 0 {
 		n += 1 + sovStoredGame(uint64(m.Wager))
+	}
+	l = len(m.Denom)
+	if l > 0 {
+		n += 1 + l + sovStoredGame(uint64(l))
 	}
 	return n
 }
@@ -694,6 +713,38 @@ func (m *StoredGame) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
+		case 12:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Denom", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowStoredGame
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthStoredGame
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthStoredGame
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Denom = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipStoredGame(dAtA[iNdEx:])