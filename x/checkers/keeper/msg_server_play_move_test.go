francesco-defalco/b9-checkers-academy-draft

Add leaderboard handling and tests.

x/checkers/keeper/msg_server_play_move_test.go
@@ -32,6 +32,15 @@ func setupMsgServerWithOneGameForPlayMove(t testing.TB) (types.MsgServer, keeper
 	return server, *k, context, ctrl, bankMock
 }
 
+func setupMsgServerWithOneGameForPlayMoveAndHooks(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context,
+	*gomock.Controller, *testutil.MockBankEscrowKeeper, *testutil.MockCheckersHooks) {
+	msgServer, k, context, ctrl, escrow := setupMsgServerWithOneGameForPlayMove(t)
+	hookMock := testutil.NewMockCheckersHooks(ctrl)
+	k.SetHooks(hookMock)
+	msgServer = keeper.NewMsgServerImpl(k)
+	return msgServer, k, context, ctrl, escrow, hookMock
+}
+
 func TestPlayMove(t *testing.T) {
 	msgServer, _, context, ctrl, escrow := setupMsgServerWithOneGameForPlayMove(t)
 	defer ctrl.Finish()
@@ -593,4 +602,25 @@ func TestPlayerInfoNoUpdatedOnNoWinner(t *testing.T) {
 		LostCount:      0,
 		ForfeitedCount: 0,
 	}, carolInfo)
+}
+
+func TestPlayerInfoNoHookOnNoWinner(t *testing.T) {
+	msgServer, keeper, context, ctrl, escrow, _ := setupMsgServerWithOneGameForPlayMoveAndHooks(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	escrow.ExpectAny(context)
+	keeper.SetPlayerInfo(ctx, types.PlayerInfo{
+		Index: bob,
+	})
+	keeper.SetPlayerInfo(ctx, types.PlayerInfo{
+		Index: carol,
+	})
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
 }

b9lab/checkers

x/checkers/keeper/msg_server_play_move_test.go
@@ -38,6 +38,16 @@ func setupMsgServerWithOneGameForPlayMoveWithMock(t testing.TB) (types.MsgServer
 	return server, *k, context, ctrl, bankMock
 }
 
+func setupMsgServerWithOneGameForPlayMoveAndHooks(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context,
+	*gomock.Controller, *testutil.MockCheckersHooks) {
+	msgServer, k, context, ctrl, escrow := setupMsgServerWithOneGameForPlayMoveWithMock(t)
+	escrow.ExpectAny(context)
+	hookMock := testutil.NewMockCheckersHooks(ctrl)
+	k.SetHooks(hookMock)
+	msgServer = keeper.NewMsgServerImpl(k)
+	return msgServer, k, context, ctrl, hookMock
+}
+
 func TestPlayMove(t *testing.T) {
 	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
 	playMoveResponse, err := msgServer.PlayMove(context, &types.MsgPlayMove{
@@ -567,4 +577,24 @@ func TestPlayerInfoNoUpdatedOnNoWinner(t *testing.T) {
 		LostCount:      0,
 		ForfeitedCount: 0,
 	}, carolInfo)
+}
+
+func TestPlayerInfoNoHookOnNoWinner(t *testing.T) {
+	msgServer, keeper, context, ctrl, _ := setupMsgServerWithOneGameForPlayMoveAndHooks(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	keeper.SetPlayerInfo(ctx, types.PlayerInfo{
+		Index: bob,
+	})
+	keeper.SetPlayerInfo(ctx, types.PlayerInfo{
+		Index: carol,
+	})
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
 }